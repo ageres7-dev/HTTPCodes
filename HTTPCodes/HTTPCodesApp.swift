ef9f1a9476db8fd23a01f59fcdad45bd8826730b
Sergey Dolgikh <ageres7@gmail.com>
2024-06-27 15:19:31 +0500

    Initial Commit

---
+++ HTTPCodes/HTTPCodesApp.swift
@@ -0,0 +1,17 @@
+//
+//  HTTPCodesApp.swift
+//  HTTPCodes
+//
+//  Created by Sergey Dolgikh on 27.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct HTTPCodesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
